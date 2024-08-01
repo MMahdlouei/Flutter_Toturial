185f742bcb31ad98663c81cdb6e4a6c4879aa9be
Majid <mahdlooyi@gmail.com>
2024-08-01 22:03:25 +0330

    Change to statefull widget and some functions added

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
 
-  @override 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int buttName = 0;
+  int currIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,         
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.lightBlue[600],
-          title: const Text("MyApp Bar"),
-          ),
-          body: const Text("MyBody"),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const[
-              BottomNavigationBarItem(
-                label: "Home",
-                icon: Icon(Icons.home)
-                ),
-              BottomNavigationBarItem(
-                label: "Settings",
-                icon: Icon(Icons.settings)
-                )
-            ],
+          title: const Center(
+            child: Text("MyApp Bar"),
           ),
         ),
+        body: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.yellow,
+              backgroundColor: Colors.cyan, // foreground
+            ),
+            onPressed: () {
+              setState(() {
+                buttName++;
+              });
+
+              print(buttName.toString());
+            },
+            child: Text(buttName.toString()),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: "Home2",
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          selectedItemColor: Colors.blueAccent,
+          currentIndex: currIndex,
+          onTap: (int index) {
+            setState(() {
+              currIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
